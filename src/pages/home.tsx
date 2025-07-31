f7a5c7004b62f85402402a066b19026d444f86b3
jin <wlsdnjs3225@naver.com>
2025-07-31 20:50:04 +0900

    intro.css-Modify

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,11 @@
+import Contents from "./section/contents";
 import Intro from "./section/intro";
 
 export default function Home() {
   return (
     <>
       <Intro />
+      <Contents />
       {/* 나중에 다른 섹션도 여기에 추가하면 됨 */}
     </>
   );
---
+++ src/pages/section/contents.tsx
@@ -0,0 +1,15 @@
+export default function Contents() {
+    return(
+        <section className="contents">
+            <article className="IT">
+                <p>아아아메리카노</p>
+            </article>
+            <article className="japan">
+                <p>아아아메리카노</p>
+            </article>
+            <article className="culture">
+                <p>아아아메리카노</p>
+            </article>
+        </section>
+    );
+}
