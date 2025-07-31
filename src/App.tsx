ae9bdabd68850d1ff7033a82798c0f73966c7ad2
jin <wlsdnjs3225@naver.com>
2025-07-31 20:02:32 +0900

    nav-bar clear

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/header";
 import Footer from "./components/footer";
 import AppRoutes from "./routes/routes";
+import NavBar from "./components/nav-bar";
 
 function App() {
   return (
     <>
       <Header />
+      <NavBar/>
       <main>
         <AppRoutes />
       </main>
