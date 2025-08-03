b483af8d549ec7981178772329339753d81a87cf
jin <wlsdnjs3225@naver.com>
2025-08-04 08:56:56 +0900

    dummy data-Modify

--- src/pages/section/culture-board-page.tsx
+++ src/pages/section/culture-board-page.tsx
@@ -1,7 +1,9 @@
 import Board from "../../components/board";
 // 중앙 데이터 파일에서 필요한 데이터를 가져옵니다.
-import { cultureBoardData, itCategories } from "../../data/_data";
+import { cultureBoardData, cultureCategories } from "../../data/_data";
 
 export default function CultureBoardPage() {
-    return <Board boardData={cultureBoardData} categories={itCategories} />;
+    return (
+        <Board boardData={cultureBoardData} categories={cultureCategories} />
+    );
 }
--- src/pages/section/japanese-board-page.tsx
+++ src/pages/section/japanese-board-page.tsx
@@ -1,7 +1,7 @@
 import Board from "../../components/board";
 // 중앙 데이터 파일에서 필요한 데이터를 가져옵니다.
-import { japaneseBoardData, itCategories } from "../../data/_data";
+import { japaneseBoardData, japaneseCategories } from "../../data/_data";
 
 export default function JapaneseBoardPage() {
-    return <Board boardData={japaneseBoardData} categories={itCategories} />;
+    return <Board boardData={japaneseBoardData} categories={japaneseCategories} />;
 }
