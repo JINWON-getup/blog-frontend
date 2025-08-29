caa78fdc21b9c5c00987aad99cc798e54b7277e6
jin <wlsdnjs3225@naver.com>
2025-08-29 16:55:01 +0900

    코드 정렬

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -5,11 +5,11 @@ import Blog from "../pages/Blog";
 import AdminLogin from "../pages/Admin/AdminLogin";
 import AdminDashboard from "../pages/Admin/AdminDashboard";
 import CreatePost from "../components/forms/CreatePost";
-import PostDetail from "../pages/Post/PostDetail";
-import BoardPage from "../pages/Board/BoardPage";
 import Login from "../pages/Auth/Login";
 import Register from "../pages/Auth/Register";
 import MyPage from "../pages/MyPage";
+import PostDetail from "../pages/Post/PostDetail";
+import BoardPage from "../pages/Board/BoardPage";
 
 export default function AppRoutes() {
     return (
@@ -17,14 +17,14 @@ export default function AppRoutes() {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/blog" element={<Blog />} />
-            <Route path="/:boardType" element={<BoardPage />} />
             <Route path="/adminLogin" element={<AdminLogin />} />
             <Route path="/admin-dashboard" element={<AdminDashboard />} />
             <Route path="/write" element={<CreatePost />} />
-            <Route path="/post/:id" element={<PostDetail />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Register />} />
             <Route path="/my-page" element={<MyPage />} />
+            <Route path="/post/:id" element={<PostDetail />} />
+            <Route path="/:boardType" element={<BoardPage />} />
         </Routes>
     );
 }
